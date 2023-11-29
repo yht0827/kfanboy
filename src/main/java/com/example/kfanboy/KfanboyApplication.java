486ea0f195d35e7630d1af998e6580e378cd7ce0
yht0827 <yangheetae0827@naver.com>
2023-11-29 09:51:11 +0900

    Initial commit

---
+++ src/main/java/com/example/kfanboy/KfanboyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kfanboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfanboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfanboyApplication.class, args);
+	}
+
+}
