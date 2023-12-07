92205600661350b17fc44522b23799b6b83b894b
yht0827 <yangheetae0827@naver.com>
2023-12-07 21:06:44 +0900

    [#3] 스프링 부트 전역 설정
    - 공통 ResponseEntity 작업
    - 에러 처리(Exception Handler)
    - Jpa auditing 설정
    - 공통 사용 메세지 추가

---
+++ src/main/java/com/example/kfanboy/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.kfanboy.global.exception;
+
+import static com.example.kfanboy.global.common.response.ResponseHandler.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.kfanboy.global.common.response.HttpResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(CustomException.class)
+	protected ResponseEntity<HttpResponse<?>> handleCustomException(final CustomException exception) {
+		return error(exception);
+	}
+
+	/**
+	 * Validation Exception
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<HttpResponse<?>> handleValidException(
+		final MethodArgumentNotValidException exception) {
+		FieldError error = exception.getBindingResult().getFieldErrors().get(0);
+		return error(HttpStatus.BAD_REQUEST, error.getField() + " : " + error.getDefaultMessage());
+	}
+}
