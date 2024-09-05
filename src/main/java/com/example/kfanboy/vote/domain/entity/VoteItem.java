5a0e2e47ffd1c3ca45ce74a34694e14074a58a25
yht0827 <yangheetae0827@naver.com>
2024-09-05 11:19:12 +0900

    [#40] 투표 엔티티 작성
    - voteItem 엔티티 추가

--- src/main/java/com/example/kfanboy/vote/domain/entity/VoteItem.java
+++ src/main/java/com/example/kfanboy/vote/domain/entity/VoteItem.java
@@ -1,4 +1,52 @@
 package com.example.kfanboy.vote.domain.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "vote_item")
+@SQLRestriction("deleted_at is null")
+@SQLDelete(sql = "update vote_item set deleted_at=current_timestamp where vote_item_id = ?")
 public class VoteItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "vote_item_id")
+	private Long voteItemId;
+
+	@Column(name = "item_content", nullable = false)
+	private String itemContent;
+
+	@Column(name = "item_count", nullable = false)
+	private Long itemCount;
+
+	@Column(name = "vote_id", nullable = false)
+	private Long voteId;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	@Builder
+	public VoteItem(Long voteItemId, String itemContent, Long itemCount, Long voteId, LocalDateTime deletedAt) {
+		this.voteItemId = voteItemId;
+		this.itemContent = itemContent;
+		this.itemCount = itemCount;
+		this.voteId = voteId;
+		this.deletedAt = deletedAt;
+	}
 }
--- src/main/resources/db/migration/V1.4__create_table_vote_and_vote_item.sql
+++ src/main/resources/db/migration/V1.4__create_table_vote_and_vote_item.sql
@@ -1,14 +1,23 @@
 CREATE table vote
 (
-    vote_id     bigint       not null auto_increment,
-    title       varchar(100) not null,
-    vote_count  integer      not null,
-    is_finished boolean      not null,
+    vote_id     bigint      not null auto_increment,
+    title       varchar(50) not null,
+    vote_count  integer     not null,
+    is_finished boolean     not null,
     start_at    datetime(6)  not null default now(6),
     end_at      datetime(6)           default null,
     created_at  datetime(6)  not null default now(6),
     updated_at  datetime(6)  not null default now(6),
     deleted_at  datetime(6)           default null,
-    member_id   bigint       not null,
+    member_id   bigint      not null,
     primary key (vote_id)
+);
+
+CREATE table vote_item
+(
+    vote_item_id bigint      not null auto_increment,
+    item_content varchar(50) not null,
+    item_count   integer     not null,
+    vote_id      bigint      not null,
+    primary key (vote_item_id)
 );
